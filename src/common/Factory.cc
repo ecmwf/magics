f3349077544acca8fc13f8abd6617b9e452050ff
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2013-09-15 22:12:31 +0100

    add debug info

--- src/common/Factory.cc
+++ src/common/Factory.cc
@@ -65,7 +65,7 @@ B* SimpleFactory<B>::create(const string& name)
 #ifdef MAGICS_EXCEPTION
 	throw NoFactoryException(name);
 #else 
-    cerr << "SimpleFactory: Failed to create an object named " << name << endl;
+    cerr << "SimpleFactory: Failed to create an object named '" << name << "'" << endl;
 	return 0;
 #endif	
 } 
@@ -82,7 +82,7 @@ SimpleFactory<B>* SimpleFactory<B>::get(const string& name)
 #ifdef MAGICS_EXCEPTION
 	throw NoFactoryException(name);
 #else 
-    cerr << "SimpleFactory: Failed to get an object named " << name << endl;
+    cerr << "SimpleFactory: Failed to get an object named '" << name << "'" << endl;
 	return 0;
 #endif
 }
