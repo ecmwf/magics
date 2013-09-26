4e9a96d3459603f723373fea8606f91074d86d6b
Sylvie Lamy-Thepaut <cgs@ecmwf.int>
2013-09-26 10:57:47 +0100

    More on regression

--- regression/compare.py
+++ regression/compare.py
@@ -9,14 +9,17 @@ def main(args=None):
     if args == None:
             args = sys.argv
     print args
+    if os.path.exists(args[2]):
+		os.remove(args[2])
+		print "Clean File %s before running the test" % ( args[2] )
     e = call([args[1]])
     e = os.path.exists(args[2])
-    print e 
+	
     if ( e == False ) :
 		print "File %s has not been generated" % ( args[2] )
-    
-
-    sys.exit(e)
+		sys.exit(1)
+    print "File %s has been generated" % ( args[2] )
+    sys.exit(0)
 
 
 if __name__ == "__main__":
