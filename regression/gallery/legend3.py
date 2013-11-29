3783c692bfc8a5a7edabe1c942a693de3de6b1db
Sylvie Lamy-Thepaut <cgs@ecmwf.int>
2013-11-29 13:31:06 +0000

    More Regression tests

---
+++ regression/gallery/legend3.py
@@ -0,0 +1,92 @@
+
+#importing Magics module
+from Magics.macro import *
+
+#Example reference
+ref = 'legend3'
+
+#Setting of the output file name
+output = output(output_formats= ['png'],
+                output_name_first_page_number= "off",
+                output_name= ref)
+
+#Setting the coordinates of the geographical area
+europe = mmap(subpage_upper_right_longitude= 65.,
+              subpage_map_projection= "polar_stereographic",
+              subpage_map_vertical_longitude= 0.0,
+              subpage_lower_left_longitude= -37.27,
+              subpage_lower_left_latitude= 21.51,
+              subpage_upper_right_latitude= 51.28)
+
+#Coastlines setting
+coast = mcoast( map_grid= "on",
+                map_grid_colour= "tan",
+                map_coastline_colour= "tan",
+                map_coastline_resolution= "high")
+
+#Import the z500 data 
+z500 =  mgrib(grib_input_file_name = "./z500.grb",
+              grib_id= "z500")
+
+#Define the simple contouring for z500
+z500_contour = mcont(legend= "off",
+                     contour_level_selection_type= "interval",
+                     contour_line_colour= "black",
+                     contour_hilo_height= 0.25,
+                     contour_line_thickness= 1,
+                     contour_hilo= "on",
+                     contour_hilo_quality= "high",
+                     contour_label= "off",
+                     contour_highlight_colour= "black",
+                     contour_highlight_thickness= 2,
+                     contour_interval= 5.)
+
+#Import the t850 data 
+t850 =  mgrib( grib_input_file_name = "./t850.grb",
+               grib_id= "t850")
+
+
+#Define the shading for t850
+t850_contour = mcont(legend="on",
+		     contour_shade= "on",
+		     contour_hilo= "off",
+		     contour= "off",
+		     contour_label= "off",
+		     contour_shade_method= "area_fill",
+		     contour_level_selection_type = "level_list",
+		     contour_level_list = [-10.0,-8.0,-6.0,-4.0,-2.0,0.0,2.0,4.0,6.0,8.0,10.0],
+		     contour_shade_max_level_colour= "red",
+		     contour_shade_min_level_colour= "blue",
+		     contour_shade_colour_direction= "clockwise")
+
+    
+
+
+title = mtext(text_lines = ["<font size='1'>Positional and histogram legend</font>","","","","",""],
+	      text_justification = "left",
+	      text_font_size = 0.5,
+	      text_colour = "charcoal")
+
+#add a legend
+legend = mlegend(legend = "on",
+ 		 legend_text_colour="black",
+		 legend_box_mode= "positional",
+		 legend_box_x_position= 1.0,
+		 legend_box_y_position= 16.0,
+		 legend_box_x_length= 27.,
+		 legend_box_y_length= 3., 		 
+		 legend_display_type = "histogram",
+		 legend_label_frequency = 1,
+		 legend_histogram_mean_value = "on",
+		 legend_title = "on",
+		 legend_title_text= "Temperature at 850 hPa",
+		 legend_text_font_size = 0.5)
+
+#To the plot
+plot(output, europe, t850, t850_contour, z500, z500_contour, coast, title, legend)
+
+#For documentation only
+tofortran(ref, output, europe, t850, t850_contour, z500, z500_contour, coast, title, legend)
+tomv4(ref,legend)
+tohtml(ref,legend)
+
---
+++ regression/gallery/symbol3.py
@@ -0,0 +1,71 @@
+
+#importing Magics module
+from Magics.macro import *
+
+
+ref = 'symbol3'
+#Setting of the output file name
+output = output(output_formats = ['png'], 
+		output_name_first_page_number = "off",
+		output_name = ref)
+
+#Setting the coordinates of the geographical area
+#Here we use Europe
+area = mmap(subpage_lower_left_latitude= 40.,
+            subpage_lower_left_longitude= -20.,
+			subpage_upper_right_latitude= 65.,
+			subpage_upper_right_longitude= 10.)
+
+#Background Coastlines 
+background = mcoast( map_coastline_sea_shade_colour = "white",
+                     map_coastline_land_shade_colour = "cream",
+                     map_grid = "on",
+                     map_coastline_land_shade = "on",
+                     map_coastline_sea_shade = "on",
+                     map_label = "on",
+                     map_coastline_colour = "tan")
+
+
+
+#Import the input data 
+input =  mgeo(geo_input_file_name = "input.geo")
+
+#Define the symbol plotting
+symbol = msymb( legend = "off", 
+			symbol_type =  "marker",
+		    symbol_colour =  "orange", 
+			symbol_height =  1.,
+			symbol_marker_mode = "name", 
+			symbol_marker_name = "ww_80")
+
+lines =["Using Shower Symbol"]
+
+title = mtext({
+           "text_lines" : lines,
+           "text_html" : "true",
+		   "text_justification": "left",
+		   "text_font_size": 1.,
+           "text_colour" : "charcoal"})
+
+
+#To the plot
+plot(output, area, background, input, symbol, title)
+
+#For documentation only
+tofortran(ref, output, area, background, input, symbol, title)
+tomv4(ref, symbol)
+tohtml(ref, symbol)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
