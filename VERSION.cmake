abf647120373d7ebfe999b3836195b6e10a071c0
Sylvie Lamy-Thepaut <cgs@ecmwf.int>
2013-12-02 15:55:14 +0000

    ecgb

--- VERSION.cmake
+++ VERSION.cmake
@@ -5,6 +5,6 @@ EXECUTE_PROCESS(
         COMMAND sh -c "cd ${CMAKE_SOURCE_DIR}; git rev-parse --abbrev-ref HEAD")
         )
 debug_var (x)
-set( ${PROJECT_NAME}_VERSION_STR  "2.21.0-rc1" )
+set( ${PROJECT_NAME}_VERSION_STR  "2.21.1-bufr" )
 
 debug_var(${PROJECT_NAME}_VERSION_STR)
