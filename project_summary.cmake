eee7817972bcd022266d5c0b51810585ffa40907
Tiago Quintino <tiago.quintino@ecmwf.int>
2013-12-10 14:35:57 +0000

    improvement of buidl system with searhc for numpy

--- project_summary.cmake
+++ project_summary.cmake
@@ -2,6 +2,11 @@ if(Boost_FOUND)
   message( STATUS " Boost include : [${Boost_INCLUDE_DIRS}]" )
 endif()
 
+if( SWIG_FOUND AND NUMPY_FOUND )
+  message( STATUS " Swig (${SWIG_VERSION}) : [${SWIG_EXECUTABLE}]" )
+  message( STATUS " NumPy (${NUMPY_VERSION}) : [${NUMPY_INCLUDE_DIRS}]" )
+endif()
+
 if(PROJ4_FOUND)
   message( STATUS " Proj4 include : [${PROJ4_INCLUDE_DIRS}]" )
   message( STATUS "         libs  : [${PROJ4_LIBRARIES}]" )
