0113c18b42e95c667c4b5000326cd31196bb28d0
Tiago Quintino <tiago.quintino@ecmwf.int>
2013-12-10 14:35:31 +0000

    use sha1 from ecbuild

--- VERSION.cmake
+++ VERSION.cmake
@@ -1,10 +1,6 @@
+if( ${PNAME}_GIT_SHA1_SHORT )
+	set( XX "-${${PNAME}_GIT_SHA1_SHORT}" )
+endif()
 
-execute_process(
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        COMMAND  git rev-parse --abbrev-ref HEAD
-        OUTPUT_VARIABLE XX)
-
-STRING(REGEX REPLACE "\n" ""  XX ${XX})
-set( ${PROJECT_NAME}_VERSION_STR  "2.21.0-${XX}" )
-set( BRANCH_NAME  "2.21.0-${XX}" )
-
+set( ${PROJECT_NAME}_VERSION_STR  "2.21.0${XX}" )
+set( BRANCH_NAME  ${${PROJECT_NAME}_VERSION_STR} )
