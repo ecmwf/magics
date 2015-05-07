bdf268c623524b4fe4be93ff7d1959da85ad1057
Stephan Siemen <stephan.siemen@ecmwf.int>
2015-05-07 10:38:47 +0100

    VERSION - updated to 2.24.6

--- VERSION.cmake
+++ VERSION.cmake
@@ -1,5 +1,5 @@
-set ( metabuilder_version 2.24.5 )
-set ( _version 2.24.5 )
+set ( metabuilder_version 2.24.6 )
+set ( _version 2.24.6 )
 if ( MAGICS_BUILD )
     set( ${PROJECT_NAME}_VERSION_STR  "${_version}-${MAGICS_BUILD}" )   
 else ()
