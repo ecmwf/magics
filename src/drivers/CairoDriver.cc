f410220552e64170c918229390dca7dfb5012ee7
Stephan Siemen <stephan.siemen@ecmwf.int>
2015-06-05 11:11:39 +0100

    DRIVERS - Cairo revert unintended change  MAGP-657

--- src/drivers/CairoDriver.cc
+++ src/drivers/CairoDriver.cc
@@ -1144,7 +1144,7 @@ MAGICS_NO_EXPORT void CairoDriver::renderSimplePolygon() const
                                  << "             Solid shading used instead."<< std::endl;
 #endif
 	{
-/*        if(cairo_get_antialias(cr_) != CAIRO_ANTIALIAS_NONE && currentColour_.alpha() > 0.9999 )
+        if(cairo_get_antialias(cr_) != CAIRO_ANTIALIAS_NONE && currentColour_.alpha() > 0.9999 )
 	    //if(magCompare(backend_,"png")) // if(cairo_get_antialias(cr_) != CAIRO_ANTIALIAS_NONE && currentColour_.alpha() > 0.9999 )
 	    {
 	        cairo_fill_preserve(cr_);
@@ -1152,7 +1152,6 @@ MAGICS_NO_EXPORT void CairoDriver::renderSimplePolygon() const
 	        cairo_stroke(cr_);
 	    }
 	    else
-*/
 	    {
 	        cairo_fill(cr_);
 	    }
