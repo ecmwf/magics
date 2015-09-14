fbeae0c52a173318c2f5c734a30aaae792c3cbdf
Sylvie Lamy-Thepaut <Sylvie.Lamy-Thepaut@ecmwf.int>
2015-09-14 14:25:52 +0100

    Start working on 2.25.3

--- VERSION.cmake
+++ VERSION.cmake
@@ -1,6 +1,6 @@
 
-set ( metabuilder_version 2.25.2 )
-set ( _version 2.25.2 )
+set ( metabuilder_version 2.25.3 )
+set ( _version 2.25.3 )
 if ( MAGICS_BUILD )
     set( ${PROJECT_NAME}_VERSION_STR  "${_version}-${MAGICS_BUILD}" )   
 else ()
