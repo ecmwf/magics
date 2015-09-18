5c9c70cd021545e43fd6fe68a99a5cf973d29e97
Sylvie Lamy-Thepaut <Sylvie.Lamy-Thepaut@ecmwf.int>
2015-09-18 16:50:47 +0100

    Streamlines : Fix few issues [MAGP-253]

--- regression/wind/global_streamlines.py
+++ regression/wind/global_streamlines.py
@@ -12,11 +12,12 @@ output = output(output_formats= ['png'],
 
 #Setting the coordinates of the geographical area
 area = mmap(subpage_upper_right_longitude= 60.,
-                 subpage_upper_right_latitude= 20.,
-                 subpage_lower_left_longitude= 20.,
+                 subpage_upper_right_latitude= 60.,
+                 subpage_lower_left_longitude= -20.,
                  subpage_map_projection= 'cylindrical',
                  subpage_lower_left_latitude= 0.)
 
+polar = mmap( subpage_map_projection= 'polar_stereographic',)
 #Background Coastlines 
 background = mcoast( map_coastline_sea_shade_colour= 'white',
                      map_coastline_land_shade_colour= 'cream',
@@ -36,12 +37,17 @@ foreground = mcoast(  map_grid= 'on',
 
 
 #Import the  wind  at 200hPa uv200 
-uv200 =  mgrib( grib_input_file_name = './uv500.grb',grib_id= 'uv200')
+uv200 =  mgrib( grib_input_file_name = './uv.grib',grib_id= 'uv200')
 
 streamlines = mwind(
                 legend= 'on',
                 wind_field_type = 'streamlines',
+                wind_streamline_min_density =7.,
+                wind_streamline_thickness =1,
                 )
+wind = mwind(wind_arrow_unit_velocity = 50.00,
+  wind_thinning_factor = 5.00
+  )
 
 title = mtext( text_lines = ["<font size='1'>Streamlines</font>"],
 	       text_justification = 'left',
@@ -52,7 +58,7 @@ title = mtext( text_lines = ["<font size='1'>Streamlines</font>"],
 legend = mlegend(legend= 'on',
            legend_text_colour= 'charcoal',
            legend_box_mode= 'positional',
-           legend_box_x_position= 27.5,
+           legend_box_x_position= 26.5,
            legend_box_y_position= 4.,
            legend_box_x_length= 2.,
            legend_box_y_length= 12.,
@@ -65,5 +71,5 @@ legend = mlegend(legend= 'on',
 	   legend_text_font_size = '0.5')
 
 #To the plot
-plot(output, background,uv200, streamlines,foreground, title, legend)
+plot(output, background,uv200, streamlines,uv200, wind, foreground, title)
 
