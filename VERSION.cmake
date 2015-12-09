c49a8a0f85031cb14988a4a717df1ebcb512680f
Sylvie Lamy-Thepaut <Sylvie.Lamy-Thepaut@ecmwf.int>
2015-12-09 17:11:21 +0000

    Update Version to 2.26.2

--- VERSION.cmake
+++ VERSION.cmake
@@ -1,6 +1,6 @@
 
-set ( metabuilder_version 2.26.1 )
-set ( _version 2.26.1 )
+set ( metabuilder_version 2.26.2 )
+set ( _version 2.26.2 )
 if ( MAGICS_BUILD )
     set( ${PROJECT_NAME}_VERSION_STR  "${_version}-${MAGICS_BUILD}" )   
 else ()
