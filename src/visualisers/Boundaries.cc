9f7e3f2835e1656488169a20f72af6f2d086b5c3
Stephan Siemen <stephan.siemen@ecmwf.int>
2016-01-05 21:05:07 +0000

    COASTFILES - enable selection of admin boundaries   MAGP-435
    The column to select the boundaries were renamed from "iso" to "adm0_a3"

--- src/visualisers/Boundaries.cc
+++ src/visualisers/Boundaries.cc
@@ -83,7 +83,7 @@ void Boundaries::operator()(const map<string, string>& setting, BasicGraphicsObj
 		file = share_folder + administrative_boundaries;
 
 		admistrative.setPath(file);
-		admistrative.decode(task.transformation(),"iso", administrative_list_);
+		admistrative.decode(task.transformation(),"adm0_a3", administrative_list_);
 		const Transformation& transformation = task.transformation();
 
 		for ( ShapeDecoder::const_iterator boundary = admistrative.begin(); boundary != admistrative.end(); ++boundary)
@@ -92,7 +92,6 @@ void Boundaries::operator()(const map<string, string>& setting, BasicGraphicsObj
 			poly.setColour( administrative_colour_->automatic() ? *colour_ : *administrative_colour_);
 			poly.setThickness(administrative_thickness_);
 			poly.setLineStyle(administrative_style_);
-
 
 			(**boundary).setToFirst();
 			while ((**boundary).more())
