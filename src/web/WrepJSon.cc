3d12c07450d20c071ceb56e44948c95bb94a2f8b
Sylvie Lamy-Thepaut <Sylvie.Lamy-Thepaut@ecmwf.int>
2016-02-10 15:04:53 +0000

    IImprove management of expver token in title [MAGP-769]

--- src/web/WrepJSon.cc
+++ src/web/WrepJSon.cc
@@ -1326,13 +1326,11 @@ void WrepJSon::visit(TextVisitor& text)
 	text.update("json", "short_temperature_correction_info", short_correction.str());
 	text.update("json", "parameter_info", (param_info_ == "none") ? "": param_info_ );
 
-	if (param_info_ != "none")
+	if (param_info_ != "none") {
 		text.update("json", "station_name", station_name_);
-
-	
-    if ( !expver_.empty() && expver_ != "0001") {
-        text.update("json", "expver",  " [" + expver_ + "] ");
-    }
+		if ( !expver_.empty() && expver_ != "0001") {
+			text.update("json", "expver",  " [" + expver_ + "] ");
+	}
 
     text.update("json", "product_info", product_info_);
 	text.update("json", "plumes_interval", tostring(plumes_));
