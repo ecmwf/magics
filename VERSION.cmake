4237c84a947cfdc00c26f96cee6141ce680ad4e5
Stephan Siemen <stephan.siemen@ecmwf.int>
2016-03-11 13:07:41 +0000

    VERSION - set 2.29.0 for develop branch

--- VERSION.cmake
+++ VERSION.cmake
@@ -1,6 +1,6 @@
 
-set ( metabuilder_version 2.27.3 )
-set ( _version 2.27.3 )
+set ( metabuilder_version 2.29.0 )
+set ( _version 2.29.0 )
 if ( MAGICS_BUILD )
     set( ${PROJECT_NAME}_VERSION_STR  "${_version}-${MAGICS_BUILD}" )   
 else ()
