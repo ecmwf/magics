148bbef6179da3dacee34a20967f2965b48364e9
Stephan Siemen <stephan.siemen@ecmwf.int>
2016-06-20 17:38:10 +0100

    LICENSE - update licence code for Terralib code to be under Apache  MAGP-253
    As agreed with INPE

--- src/terralib/kernel/TeMeasure.h
+++ src/terralib/kernel/TeMeasure.h
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 1996-2016 ECMWF.
+ * (C) Copyright 1996-2016 ECMWF & INPE.
  * 
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
@@ -8,28 +8,6 @@
  * does it submit to any jurisdiction.
  */
 
-/************************************************************************************
-TerraLib - a library for developing GIS applications.
-Copyright  2001-2007 INPE and Tecgraf/PUC-Rio.
-
-This code is part of the TerraLib library.
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library.
-
-The authors reassure the license terms regarding the warranties.
-They specifically disclaim any warranties, including, but not limited to,
-the implied warranties of merchantability and fitness for a particular purpose.
-The library provided hereunder is on an "as is" basis, and the authors have no
-obligation to provide maintenance, support, updates, enhancements, or modifications.
-In no event shall INPE and Tecgraf / PUC-Rio be held liable to any party for direct,
-indirect, special, incidental, or consequential damages arising out of the use
-of this library and its documentation.
-*************************************************************************************/
 /*! \file TeMeasure.h
     \brief This file defines a class for handling a measure
 */
