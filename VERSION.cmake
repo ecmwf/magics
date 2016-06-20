d148160277279413b5aaf14ce6708a1fba7c9f3c
Stephan Siemen <stephan.siemen@ecmwf.int>
2016-06-20 08:59:07 +0100

    VERSION - update version of hotfix to 2.29.1

--- VERSION.cmake
+++ VERSION.cmake
@@ -1,6 +1,6 @@
 
-set ( metabuilder_version 2.29.0 )
-set ( _version 2.29.0 )
+set ( metabuilder_version 2.29.1 )
+set ( _version 2.29.1 )
 if ( MAGICS_BUILD )
     set( ${PROJECT_NAME}_VERSION_STR  "${_version}-${MAGICS_BUILD}" )   
 else ()
