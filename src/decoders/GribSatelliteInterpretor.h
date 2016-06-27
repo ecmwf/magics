76c88aecf1aa41ea7e03c36fb0af11129cae9857
Sylvie Lamy-Thepaut <Sylvie.Lamy-Thepaut@ecmwf.int>
2016-06-27 16:47:46 +0100

    HotFix: Fix issue with space_view representation.

--- src/decoders/GribSatelliteInterpretor.h
+++ src/decoders/GribSatelliteInterpretor.h
@@ -1,22 +1,22 @@
 /*
  * (C) Copyright 1996-2016 ECMWF.
- * 
+ *
  * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
- * In applying this licence, ECMWF does not waive the privileges and immunities 
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
  * granted to it by virtue of its status as an intergovernmental organisation nor
  * does it submit to any jurisdiction.
  */
 
 /*! \file GribSatelliteInterpretor.h
     \brief Definition of the Template class GribSatelliteInterpretor.
-    
+
     Magics Team - ECMWF 2005
-    
+
     Started: Mon 18-Apr-2005
-    
+
     Changes:
-    
+
 */
 
 #ifndef GribSatelliteInterpretor_H
@@ -31,31 +31,31 @@ namespace magics {
 class GribSatelliteInterpretor: public GribInterpretor {
 
 public:
-	GribSatelliteInterpretor();
-	virtual ~GribSatelliteInterpretor();
-	
-//	virtual void interpretAsRaster(const GribDecoder&, RasterData&, const Transformation&) const;
-    virtual void interpretAsVectors(const GribDecoder&, Matrix&, Matrix&) const 
-    	{ throw  NotYetImplemented("Satellite representation", " Vectors"); }
-    virtual void interpretAsMatrix(const GribDecoder&, Matrix**) const;
+    GribSatelliteInterpretor();
+    virtual ~GribSatelliteInterpretor();
 
-	
+//  virtual void interpretAsRaster(const GribDecoder&, RasterData&, const Transformation&) const;
+    virtual void interpretAsVectors(const GribDecoder&, Matrix&, Matrix&) const
+        { throw  NotYetImplemented("Satellite representation", " Vectors"); }
+    virtual void interpretAsMatrix(const GribDecoder&, Matrix**, Matrix** matrix2= NULL) const;
+
+
 
 protected:
      //! Method to print string about this class on to a stream of type ostream (virtual).
-	 virtual void print(ostream&) const; 
+     virtual void print(ostream&) const;
 
 private:
     //! Copy constructor - No copy allowed
-	GribSatelliteInterpretor(const GribSatelliteInterpretor&);
+    GribSatelliteInterpretor(const GribSatelliteInterpretor&);
     //! Overloaded << operator to copy - No copy allowed
     void AdjustBadlyEncodedGribs(int satId, int chanId, long &nx, long &ny, long &dx, long &dy, double &xp, double &yp, double &slon, long &functionCode) const;
 
 
 // -- Friends
     //! Overloaded << operator to call print().
-	friend ostream& operator<<(ostream& s,const GribSatelliteInterpretor& p)
-		{ p.print(s); return s; }
+    friend ostream& operator<<(ostream& s,const GribSatelliteInterpretor& p)
+        { p.print(s); return s; }
 
 };
 
