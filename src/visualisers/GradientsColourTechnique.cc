78bb25eeb4391e7b3970aa3d0495b6a9486afdb2
Sylvie Lamy-Thepaut <Sylvie.Lamy-Thepaut@ecmwf.int>
2017-03-16 10:38:36 +0000

    MAGP-928 - Add hcl colour interpolation to gradient shading

--- src/visualisers/GradientsColourTechnique.cc
+++ src/visualisers/GradientsColourTechnique.cc
@@ -40,55 +40,119 @@ void GradientsColourTechnique::set(LevelSelection& out, LevelSelection& in, Colo
 	// First make sure that 
 	ColourTableDefinitionCompute helper;
 
-
-	vector<string>::const_iterator col = colours_.begin();
-	vector<double>::const_iterator val = values_.begin();
+	
+	vector<double>::const_iterator val = stops_.begin();
 	vector<int>::const_iterator step = steps_.begin();
-	vector<string>::const_iterator technique = techniques_.begin();
+	
 	
 	if ( colours_.empty() ) {
 		MagLog::warning() << " No colours given to the gradients method" << endl;
 		return;
 	}
-	if ( values_.empty() ) {
+	if ( stops_.empty() ) {
 		MagLog::warning() << " No intervals given to the gradients method" << endl;
 		return;
 	}
-	string left = *col;
-	++col;
-	double from = *val;
-	++val;
+	
 	out.clear();
 	in.clear();
 	
-	while ( col != colours_.end() ) {
-		string right = *col;
-		double to = *val;
+  	//ColourTable colours;
+  	int last = colours_.size() -1;
+  	for ( int col = 1; col < colours_.size(); ++col) {
+  		string left = colours_[col-1];
+  		string right = colours_[col];	
+  		int istep = ( steps_.empty() ) ? 10 : *step;
+  		
+  		// right
+  		ColourTableDefinitionCompute helper(left, right, technique_, technique_direction_);
+  		
+  		int nb;
+  		if ( stop_method_ == "right") 
+  			nb = (col == 1 ) ? istep + 1 : istep + 2;
+  		else if ( stop_method_ == "left" )
+  			nb = (col == last ) ? istep + 1 : istep + 2;
+  		else if ( stop_method_ == "ignore" ) 
+  			nb = (col == 1 || col == last ) ? istep + 2 : istep + 3;
+  		else if ( stop_method_ == "both" ) 
+  			nb = istep+1;
+  		
+  		helper.set(table, nb);
 
-		int istep = ( steps_.empty() || steps_.empty() ) ? 10 : *step;
-		string stechnique = ( techniques_.empty() || technique == techniques_.end() ) ? "linear" : *technique;
-		ColourTableDefinitionCompute helper(left, right, stechnique);
-		helper.set(table, (istep)+1);
-		double increment = ((to-from)/(istep) );
-		for (int i = 0; i < istep; i++) {
-			in.push_back(from + (i*increment));
-			out.push_back(from + (i*increment));
-		}
-		left = right;
-		from = to;
-		++col;
-		
-		++val;
-		
-		if ( step != steps_.end() )
+		// Next block
+		if ( !steps_.empty() && step != steps_.end() ) 
 			++step;
-		if ( technique == techniques_.end() )
-			++technique;
 
+  	}
+	
+	
+	step = steps_.begin();
+	int col = 0;
+
+	// Now the interval ...
+  	for (int stop = 1; stop < stops_.size(); ++stop) { 
+  		  double from = stops_[stop-1];
+  		  double to = stops_[stop];
+  		  int istep = ( steps_.empty() ) ? 10 : *step;
+		  if (  stop_method_ == "ignore") {
+			
+		  	in.push_back(from);
+		  	out.push_back(from);
+		  	if ( stop != 1) {
+		  		in.push_back(from);
+		  		out.push_back(from);
+		  	}
+		  }
+		  if (  stop_method_ == "right") {
+			
+		  	in.push_back(from);
+		  	out.push_back(from);
+		  }
+		  if (  stop_method_ == "left") {
+			
+		  	in.push_back(from);
+		  	out.push_back(from);
+		  } 
+		  if (  stop_method_ == "both") {
+			
+		  	in.push_back(from);
+		  	out.push_back(from);
+		  } 
+		  nb = istep;
+		  double inc = (to - from )/(nb);
+		  for (int i = 1; i < nb; i++) {
+		  			in.push_back(from +(i*inc));
+		  			out.push_back(from +(i*inc));
+		  			
+		  		
+		  }
+		  if (  stop_method_ == "ignore") {
+			in.push_back(to);
+		  	out.push_back(to);
+		  	
+		  }	
+		  if (  stop_method_ == "right") {
+			in.push_back(to);
+		  	out.push_back(to);
+		  	
+		  }	
+		  if (  stop_method_ == "left") {
+			
+		  	in.push_back(to);
+		  	out.push_back(to);
+		  }
+		  if (  stop_method_ == "both" && stop == stops_.size()-1) {
+			in.push_back(to);
+		  	out.push_back(to);
+		  	
+		  }
+		  
+		if ( !steps_.empty() && step != steps_.end() ) 
+			++step;
 	}
-	// add the last entry ! 
-	in.push_back(from);
-	out.push_back(from);
+	
+
+
 
 	
 	// now we compute the new levels list :
