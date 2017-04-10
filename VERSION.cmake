80b19aa0509052f9c740d8f0b264077fda8224ed
Sylvie Lamy-Thepaut <Sylvie.Lamy-Thepaut@ecmwf.int>
2017-04-10 14:03:06 +0100

    Update Version to 2.33.0 + small compil error

--- VERSION.cmake
+++ VERSION.cmake
@@ -6,8 +6,8 @@
 # granted to it by virtue of its status as an intergovernmental organisation nor
 # does it submit to any jurisdiction.
 
-set ( metabuilder_version 2.32.0 )
-set ( _version 2.32.0 )
+set ( metabuilder_version 2.33.0 )
+set ( _version 2.33.0 )
 
 if ( MAGICS_BUILD )
     set( ${PROJECT_NAME}_VERSION_STR  "${_version}-${MAGICS_BUILD}" )
