d82c3f09a367ba9094c1d89438bc62c6b8433762
Sylvie Lamy-Thepaut <Sylvie.Lamy-Thepaut@ecmwf.int>
2017-08-04 15:09:39 +0100

    going on holidays !

---
+++ tools/xml2milana.py
@@ -0,0 +1,11 @@
+
+import xmltodict
+
+with open('../src/params/CoastPlotting.xml') as fd:
+	definition = xmltodict.parse(fd)
+
+print definition
+
+
+for params in definition["magics"]["class"]:
+	print params
