f4be4fbdbd215b0d05af57047c68950fd54ba372
Fernando <cgk@ecmwf.int>
2018-03-05 17:11:23 +0000

    BUFR: updated mvobs files to handle keys and descriptors [MAGP-1046]

---
+++ src/eckit_readers/MvBufrElementTable.h
@@ -0,0 +1,39 @@
+/***************************** LICENSE START ***********************************
+
+ Copyright 2012 ECMWF and INPE. This software is distributed under the terms
+ of the Apache License version 2.0. In applying this license, ECMWF does not
+ waive the privileges and immunities granted to it by virtue of its status as
+ an Intergovernmental Organization or submit itself to any jurisdiction.
+
+ ***************************** LICENSE END *************************************/
+
+#ifndef MVBUFRELEMENTTABLE_H
+#define MVBUFRELEMENTTABLE_H
+
+#include <string>
+#include <vector>
+#include <map>
+
+class MvBufrEdition;
+
+class MvBufrElementTable
+{
+public:
+   const std::string& keyName(int); //const;
+   bool buildElementTable();
+   static MvBufrElementTable* find(MvBufrEdition*);
+
+protected:
+   MvBufrElementTable(MvBufrEdition* edition);
+   ~MvBufrElementTable();
+
+    //static MvBufrCodeTable* make(int element,MvEccBufrMessage* msg);
+    //void load(const std::string& path);
+
+   MvBufrEdition* edition_;
+   std::map<int,std::string> melems_; //descriptior -> key
+
+   static std::vector<MvBufrElementTable*> tables_;
+};
+
+#endif // MVBUFRELEMENTTABLE_H
--- src/eckit_readers/MvObsSet.h
+++ src/eckit_readers/MvObsSet.h
@@ -7,16 +7,20 @@
 
  ***************************** LICENSE END *************************************/
 
-// MvObsSet.h
-
-
 #ifndef MvObsSet_DEFINED
 #define MvObsSet_DEFINED
 
 //-- <aki> temporarily for testing only; should be done via 'configure'
-#define METVIEW_PREPBUFR
+//e #define METVIEW_PREPBUFR
 
+#include <eccodes.h>
 #include "MvBufrObs.h"
+
+#include <vector>
+
+//namespace mvObs {
+   std::string simplified(const std::string&);
+//}
 
 class MvPrepBufrPrep;
 
@@ -27,100 +31,121 @@ class MvPrepBufrPrep;
  */
 class MvObsSet
 {
- friend class MvObsSetIterator;
- friend class MvBufrOut;
+   friend class MvObsSetIterator;
+   friend class MvBufrOut;
 
-	// prevent copy
-
-	MvObsSet( const MvObsSet& );
-	void operator= ( const MvObsSet& );
-	void _init( const char* aFile );
+   // Prevent copy
+   MvObsSet( const MvObsSet& );
+   void operator= ( const MvObsSet& );
+   void _init( const char* );
 
  public:
 
 //! Constructor with the input BUFR file name as an argument
-   MvObsSet( const char* fileName );
+   MvObsSet( const char* );
 
 //! Constructor with a BUFR file name and access mode as arguments
 /*! This constructor can be used for writing to a file, use
  *  access mode "w" or "a".
  */
-   MvObsSet( const char* fileName, const char* accessMode );
+   MvObsSet( const char*, const char* );
 
 #ifdef METVIEW
 //! Constructor with a BUFR request and access mode as arguments
-    MvObsSet( MvRequest&, const char* aMode = "r" );
+   MvObsSet( MvRequest&, const char* aMode = "r" );
 
 //! Returns the minimum (earliest) date found in MvObsSet
-    MvDate  minDate();
+   MvDate minDate();
 
 //! Returns the maximum (latest) date found in MvObsSet
-    MvDate  maxDate();
+   MvDate maxDate();
 #endif
 
 //! Destructor
    ~MvObsSet();
 
+//FAMI20171016 - I think ecCodes does not need this function (_maxNrSubsets).
+//               If this is the case delete it later.
 //! Set max subset count for a new file
-      void  setSubsetMax( int subsetMax );
+   void setSubsetMax( int );
+
 
 //! Add one observation report to output file
-      void  add( MvObs& anObs );
+   void add( MvObs& );
 
 //! Rewind the input file
-      void  rewind();
+   void rewind();
 
 //! Returns the number of BUFR messages in the input file
 /*! This number is smaller than returned by method obsCount()
  *  if the input file contains multisubset BUFR messages.
  */
-       int  messageCount(); // nr of BUFR-messages in a file
+   int messageCount(); // nr of BUFR-messages in a file
 
 //! Returns the number of observation reports in the input file
 /*! This number is bigger than returned by method messageCount()
  *  if the input file contains multisubset BUFR messages.
  */
-       int  obsCount();     // nr of observation messages in a BUFR-file
+   int obsCount();     // nr of observation messages in a BUFR-file
 
 //! Returns the (running) number of the current BUFR message
-       int  messageNumber(){ return (int)_msgNumber; }
+   int messageNumber() { return (int)_msgNumber; }
 
 //! Closes the input/output file
-   bool  close();
+   bool close();
 
 //! Prepares 'PrepBUFR' tables if the input file contains BUFR tables
-      bool  prepBufrFile();
+   bool prepBufrFile();
 
-            //-- hack for ObsFilter (vk 970729) --
-      void  write( MvObs& anObs ){ if(_bufrOut) _bufrOut->write( anObs ); }
+   // Writes message
+   bool write( MvObs& );
+   bool writeCompressed(MvObs *obs);
+   bool write( const void*, const size_t );
+#ifdef MV_BUFRDC_TEST
+   bool write( const char*, int );
+#endif
 
-     MvObs& firstObs(){ return firstObs_; }
+   MvObs& firstObs() { return _firstObs; }
 
- protected:
-     MvObs  next();
-   bool  Open( const char* fileName );  //, char* aMode = "r" );
-      void  write( const char* aMsg, int aMsgLen );
-      void  searchMinMaxTime();
+   // Expand message
+   void expand();
+
+protected:
+   // Advance to the next message and by default unpack the message
+   MvObs next( bool unpack=true );
+
+   bool Open( const char* );  //, char* aMode = "r" );
+   void searchMinMaxTime();
  //Logical  currentInputBufferOK() {return _IO_buffer_OK;}
 
- protected:
-     string  _IO_mode;
-       FILE* _bufrFile;
-       long  _msgNumber;
-       long  _msgCount;
-       long  _obsCount;
+protected:
+
+#ifdef MV_BUFRDC_TEST
+FILE* _bufrFile;
+char*  _message;
+long _msgLen;
+#endif
+
+   FILE*       _ecFile;
+   bool        _minMaxDone;
+   bool        _IO_buffer_OK;
+   bool        _unpacked;
+   int         _msgCount;
+   int         _obsCount;
+   long        _msgNumber;
+   std::string _IO_mode;
+
+   codes_handle* _ecH;      // handle general access to eccodes
+   MvObs         _firstObs;
    TDynamicTime  _minTime;
    TDynamicTime  _maxTime;
-    bool  _minMaxDone;
-       long  _msgLen;
-       char* _message;
-       bool  _IO_buffer_OK;
 
-  MvPrepBufrPrep* prepBufr_;
-       bool  isPrepBufrFile_;
-      MvObs  firstObs_;
+#ifdef METVIEW_PREPBUFR
+   MvPrepBufrPrep* _prepBufr;
+   bool _isPrepBufrFile;
+#endif
 
-  MvBufrOut* _bufrOut;
+   MvBufrOut* _bufrOut;
 };
 
 //------------------------------------------------------------------------------
@@ -159,6 +184,16 @@ enum ETimeFilterState
   ,kTFS_bothSet
 };
 
+
+class MvObsSetIteratorObserver
+{
+public:
+      MvObsSetIteratorObserver() {}
+      virtual void notifyObsIteratorProgress(int)=0;
+};
+
+
+
 //------------------------------------------------------------ MvObsSetIterator
 //! A class to filter a set of observation reports
 /*! This class is used to filter a file containing several
@@ -170,33 +205,32 @@ enum ETimeFilterState
 class MvObsSetIterator
 {
 //! Writes the current filtering options of 'anIter' into stream 'aStream'
- friend ostream& operator<< ( ostream& aStream, const MvObsSetIterator& anIter );
+   friend std::ostream& operator<< ( std::ostream& aStream, const MvObsSetIterator& anIter );
 
-	MvObs     current;
-	MvObsSet* ObsSet;
-
- public:
+public:
 //! Constructor with a MvObset as an argument
-	MvObsSetIterator( MvObsSet& );
+   MvObsSetIterator( MvObsSet& );
+
 //! Destructor
-	~MvObsSetIterator();
+   ~MvObsSetIterator();
 
 //! Operator that returns the next available valid observation report
 /*! Subset structure is hidden from the calling application.
  */
-	MvObs operator() () { return operator()( NR_returnObs ); }
+   MvObs operator() () { return operator()( NR_returnObs ); }
 
 //! Operator that returns the next valid observation report
 /*! Using mnemonic argument 'NR_returnMsg' accesses only first
  *  subsets in multisubset BUFR messages. See enum ENextReturn.
  */
-	MvObs operator() ( ENextReturn returnType );
+   MvObs operator() ( ENextReturn returnType );
+
 
 //! Returns the sequence number of the current BUFR message
-     int   msgNumber() { return (ObsSet ? (int)ObsSet->_msgNumber : -1); }
+   int currentMessageNumber() const;
 
 //! Returns the subset number of the current observation report in the BUFR message
-     int   subsetNumber() { return current.subsetNumber(); }
+   int subsetNumber() { return current.subsetNumber(); }
 
 //! Defines where the date/time for filtering should be taken from
 /*! Date and time information is always available as metadata in
@@ -208,20 +242,20 @@ class MvObsSetIterator
  *  but by calling this method it is possible to change to use date/time
  *  from the data.
  */
-     void  useObsTime( bool b = true ){ useObsTime_ = b; }
+   void useObsTime( bool b = true ) { useObsTime_ = b; }
 
 //! Set the filtering criteria for a single date/time
-     void  setTime( const TDynamicTime& anObsTime );
+   void setTime( const TDynamicTime& );
 
 //! Set the filtering criteria for a date/time range
 /*! Observation reports that have their data/time within the range
  *  'anObsTime'-'deltaInMinutes' and 'anObsTime'+'deltaInMinutes'
  *  will be selected.
  */
-     void  setTimeRange( const TDynamicTime& anObsTime, short deltaInMinutes );
+   void setTimeRange( const TDynamicTime&, short );
 
 //! Set the filtering criteria for a date/time range
-     void  setTimeRange( const TDynamicTime& aBeginTime, const TDynamicTime& anEndTime );
+   void setTimeRange( const TDynamicTime&, const TDynamicTime& );
 
 //! Set the filtering criteria for time range only (date ignored)
 /*! Observation reports that have their time stamp between 'aBeginTime'
@@ -229,45 +263,55 @@ class MvObsSetIterator
  *  Time format is HHMM, i.e. 100*hours+minutes. Thus the value 1500
  *  signifies 3PM, and the value 15 signifies 15 mins past midnight.
  */
-     void  setTimeRangeWithoutDate( int aBegin, int anEnd );
+   void setTimeRangeWithoutDate( int, int );
 
 //! Set the filtering criteria for accepted WMO block numbers
 /*! This method can be called several times in order to set
  *  more than one accepted WMO block, i.e. WMO blocks are
  *  combined with logical OR operator.
  */
-     void  setWmoBlock( int aWmoBlock );
+   void setWmoBlock(int);
 
 //! Set the filtering criteria for accepted WMO station numbers
 /*! This method can be called several times in order to set
  *  more than one accepted WMO station, i.e. WMO stations are
  *  combined with logical OR operator.
  */
-     void  setWmoStation( long aWmoStation );
+   void setWmoStation( long );
 
 //! Set the filtering criteria for an accepted area
 /*! Observation report locations must be within the given area.
  */
-     void  setArea( const MvLocation& aCorner1, const MvLocation& aCorner2 );
+   void setArea( const MvLocation&, const MvLocation& );
 
 //! Set filtering criteria for a proximity of a cross section line
-     void  setXSectionLine( const MvLocation& aStartPoint
-			  , const MvLocation& anEndPoint
-			  , float aDeltaInMeters );
+   void setXSectionLine( const MvLocation&, const MvLocation&, float );
 
 //! Set the filtering criteria for accepted BUFR message types
 /*! This method can be called several times in order to set
  *  more than one accepted BUFR message type, i.e. message types are
  *  combined with logical OR operator.
  */
-     void  setMessageType( int aMsgType );
+   void setMessageType( int );
 
 //! Set the filtering criteria for accepted BUFR message local subtypes
 /*! This method can be called several times in order to set
  *  more than one accepted BUFR message local subtype, i.e. message subtypes
  *  are combined with logical OR operator.
  */
-     void  setMessageSubtype( int aMsgSubtype );
+   void setMessageSubtype( int );
+
+  void setMessageNumber(int);
+  void setEditionNumber(int);
+  void setOriginatingCentre(int);
+  void setOriginatingCentreAsStr(const std::string&);
+  void setOriginatingSubCentre(int);
+  void setMasterTableVersion(int);
+  void setLocalTableVersion(int);
+
+  void setHeaderIdent(const std::string&);
+  void setIdentKey(const std::string&);
+  void setIdentValue(const std::string&);
 
 //! Set the filtering criteria for data 'aDescriptor' having value 'aValue'
 /*! Observation reports where the data corresponding to descriptor 'aDescriptor'
@@ -275,62 +319,83 @@ class MvObsSetIterator
  *  This method can be called several times in order to set more than one
  *  accepted value for the same 'aDescriptor', i.e. values are
  *  combined with logical OR operator.
-
  */
-     void  select( long aDescriptor
-		 , float aValue );
-
+   void select( const std::string&, double );
 
 //! Set the filtering criteria for the given data having its value within a range
 /*! Observation reports where the data corresponding to descriptor 'aDescriptor'
  *  has its value in the range 'firstValue'...'secondValue' will be selected.
  */
-     void  selectRange( long aDescriptor
-		      , float firstValue
-		      , float secondValue );
+   void selectRange( const std::string&, double, double );
 
 //! Set the filtering criteria for the given data having its value outside a range
 /*! Observation reports where the data corresponding to descriptor 'aDescriptor'
  *  has its value outside the range 'firstValue'...'secondValue' will be selected.
  */
-     void  excludeRange( long aDescriptor
-		       , float firstValue
-		       , float secondValue );
+   void excludeRange( const std::string&, double, double );
+
+   //Set the observer
+   void setObserver(MvObsSetIteratorObserver* observer) {observer_=observer;}
+   void removeObserver() {observer_=0;}
+
+protected:
+   bool   checkOptionSize(std::size_t num,const std::string& functionName);
+   void   next();
+   bool   AcceptedObs( MvObs& ) const;
+   bool   TimeOk( MvObs* ) const;
+   bool   WmoBlockOk( MvObs* ) const;
+   bool   WmoStationOk( MvObs* ) const;
+   bool   WithinXSectionLine( MvObs* ) const;
+   bool   messageNumberOk( MvObs* ) const;
+   bool   msgTypeOk( MvObs* ) const;
+   bool   msgSubtypeOk( MvObs* ) const;
+   bool   editionNumberOk(MvObs*) const;
+   bool   originatingCentreOk(MvObs*) const;
+   bool   originatingCentreAsStrOk(MvObs*) const;
+   bool   originatingSubCentreOk(MvObs*) const;
+   bool   masterTableVersionOk(MvObs*) const;
+   bool   localTableVersionOk(MvObs*) const;
+   bool   headerIdentOk(MvObs*) const;
+   bool   identValueOk(MvObs*) const;
+   bool   InsideArea( MvObs* ) const;
+   bool   selectOk( MvObs* ) const;
+   float  distanceFromXSectionLine( const MvLocation& ); //-- used anywhere?
 
  protected:
+   bool _NoFiltersSet;
+   bool  useObsTime_;
+   int  _SelectValueCount;
+   int  _MsgTypeCount;
+   int  _MsgType[ MAX_FILTER_LIST_ARRAY_SIZE ];
+   int  _MsgSubtypeCount;
+   int  _MsgSubtype[ MAX_FILTER_LIST_ARRAY_SIZE ];
+   double _SelectValue[ MAX_FILTER_LIST_ARRAY_SIZE ];
+   std::string _SelectDescriptor;
+   std::vector<int> messageNumber_; //starts at 1
+   std::vector<int> editionNumber_;
+   std::vector<int> originatingCentre_;
+   std::vector<std::string> originatingCentreStr_;
+   std::vector<int> originatingSubCentre_;
+   std::vector<int> masterTableVersion_;
+   std::vector<int> localTableVersion_;
+   std::vector<int> wmoBlock_;
+   std::vector<int> wmoStation_;
+   std::vector<std::string> headerIdent_;
+   std::string identKey_;
+   std::vector<std::string> identValue_;
 
-     void  next();
-  bool  AcceptedObs( MvObs& anObs ) const;
-  bool  TimeOk( MvObs* anObs ) const;
-  bool  WmoBlockOk( MvObs* anObs ) const;
-  bool  WmoStationOk( MvObs* anObs ) const;
-  bool  WithinXSectionLine( MvObs* anObs ) const;
-  bool  msgTypeOk( MvObs* anObs ) const;
-  bool  msgSubtypeOk( MvObs* anObs ) const;
-  bool  InsideArea( MvObs* anObs ) const;
-  bool  selectOk( MvObs* anObs ) const;
-    float  distanceFromXSectionLine( const MvLocation& aPoint ); //-- used anywhere?
+   TDynamicTime fBeginTime, fEndTime;
+   ETimeFilterState _TimeFilterState;
+   ESelectFilterState _SelectState;
 
- protected:
-         bool  _NoFiltersSet;
-	    bool  useObsTime_;
-        TDynamicTime  fBeginTime, fEndTime;
-      // bool  fTimeFilterSet;
-ETimeFilterState  _TimeFilterState;
-	     int  _WmoBlockCount;
-             int  _WmoBlockNumber[ MAX_FILTER_LIST_ARRAY_SIZE ];
-	     int  _WmoStationCount;
-            long  _WmoStation[ MAX_FILTER_LIST_ARRAY_SIZE ];
-  MvXSectionLine  fXSectionLine;
-          MvArea  fArea;
-	     int  _MsgTypeCount;
-	     int  _MsgType[ MAX_FILTER_LIST_ARRAY_SIZE ];
-	     int  _MsgSubtypeCount;
-	     int  _MsgSubtype[ MAX_FILTER_LIST_ARRAY_SIZE ];
- ESelectFilterState _SelectState;
-	    long  _SelectDescriptor;
-	   float  _SelectValue[ MAX_FILTER_LIST_ARRAY_SIZE ];
-	     int  _SelectValueCount;
+   MvXSectionLine fXSectionLine;
+   MvArea fArea;
+
+private:
+   MvObs     current;
+   MvObsSet* ObsSet;
+   bool useHeaderOnly_;
+   MvObsSetIteratorObserver* observer_;
 };
 
 #endif
