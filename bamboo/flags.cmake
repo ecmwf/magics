21c1f7def482a4edc81ee2f5901721d4e7831b77
Stephan Siemen <stephan.siemen@ecmwf.int>
2018-03-21 13:54:04 +0000

    CMAKE - fix for Cmake version 2.8  MAGP-1051
    requires upper case names :-(

--- bamboo/flags.cmake
+++ bamboo/flags.cmake
@@ -1,12 +1,12 @@
 # (C) Copyright 1996-2016 ECMWF.
-# 
+#
 # This software is licensed under the terms of the Apache Licence Version 2.0
-# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
-# In applying this licence, ECMWF does not waive the privileges and immunities 
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
 # granted to it by virtue of its status as an intergovernmental organisation nor
 # does it submit to any jurisdiction.
 
-SET(ENABLE_CAIRO   OFF CACHE BOOL "Enable PNG")
+SET(ENABLE_CAIRO   ON  CACHE BOOL "Enable PNG")
 SET(ENABLE_PYTHON  ON  CACHE BOOL "Enable Python")
 SET(ENABLE_BUFR    ON  CACHE BOOL "Enable BUFR")
 SET(ENABLE_ODB     ON  CACHE BOOL "Enable ODB")
