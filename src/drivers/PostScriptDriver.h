410b33ca219b79217ec4faa8b9f4137407632def
Stephan Siemen <stephan.siemen@ecmwf.int>
2018-03-24 00:07:39 +0000

    DRIVERS - fix rotated EPS MAGP-1043

--- src/drivers/PostScriptDriver.h
+++ src/drivers/PostScriptDriver.h
@@ -1,9 +1,9 @@
 /*
  * (C) Copyright 1996-2016 ECMWF.
- * 
+ *
  * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
- * In applying this licence, ECMWF does not waive the privileges and immunities 
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
  * granted to it by virtue of its status as an intergovernmental organisation nor
  * does it submit to any jurisdiction.
  */
@@ -117,7 +117,6 @@ private:
 	mutable bool      ps_;
 	mutable bool      pdf_;
 	mutable bool      eps_;
-	mutable bool      old_;
 	mutable	unsigned int maxPathSize_;
 	mutable	unsigned int deviceColourModel_;
 	mutable Colour currentWrittenColour_;
