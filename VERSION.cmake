45f48e0a29b6cabc12fa4167c8d5aaab45cc5f3a
Sylvie Lamy-Thepaut <Sylvie.Lamy-Thepaut@ecmwf.int>
2018-04-19 10:03:12 +0100

    HotFix 3.0.4 : Pline after using an array in memory

--- VERSION.cmake
+++ VERSION.cmake
@@ -6,8 +6,8 @@
 # granted to it by virtue of its status as an intergovernmental organisation nor
 # does it submit to any jurisdiction.
 
-set ( metabuilder_version 3.0.3 )
-set ( _version 3.0.3 )
+set ( metabuilder_version 3.0.4 )
+set ( _version 3.0.4 )
 
 if ( MAGICS_BUILD )
   set( ${PROJECT_NAME}_VERSION_STR  "${metabuilder_version}-${MAGICS_BUILD}" )
