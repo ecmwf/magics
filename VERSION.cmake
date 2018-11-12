f8f5a9cf49675c060dce91c3f782fdc43a453e7b
Sylvie Lamy-Thepaut <Sylvie.Lamy-Thepaut@ecmwf.int>
2018-11-12 14:28:28 +0000

    Version  3.3.0.6

--- VERSION.cmake
+++ VERSION.cmake
@@ -7,8 +7,8 @@
 # does it submit to any jurisdiction.
 
 
-set ( metabuilder_version 3.3.0.5 )
-set ( _version 3.3.0.5 )
+set ( metabuilder_version 3.3.0.6 )
+set ( _version 3.3.0.6 )
 
 
 if ( MAGICS_BUILD )
