346a92832cbe0f7635ad40c2e14f1146ee623239
Stephan Siemen <stephan.siemen@ecmwf.int>
2018-12-01 12:37:57 +0000

    Apply license headers and contribution note in AUTHORS files  MAGP-1123

--- src/ReverseIterable.h
+++ src/ReverseIterable.h
@@ -1,5 +1,16 @@
 /*
- * ReverseIterable.h
+ * (C) Copyright 2018- ECMWF
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/*!
+ * \file ReverseIterable.h
+ * \license Apache License 2.0
  *
  *  Created on: 20 Sep 2018
  *      Author: idris
