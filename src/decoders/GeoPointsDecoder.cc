364bd89cca04dd267cc12ec23442e89438df5250
Iain Russell <Iain.Russell@ecmwf.int>
2018-12-13 09:31:23 +0000

    GEOPOINTS: support NCOLS format [MAGP-1133]

--- src/decoders/GeoPointsDecoder.cc
+++ src/decoders/GeoPointsDecoder.cc
@@ -186,59 +186,184 @@ void GeoPointsDecoder::xyv1(const string& line)
 	
 	push_back(new UserPoint(lat, lon, value, value == missing_));
 }
+
+
+void GeoPointsDecoder::ncols(const string& line, const Transformation& transformation)
+{
+    std::istringstream in(line);
+	double lat, lon, date, time, level, value;
+    string stnid;
+
+    // the column order has been detected by parseColumnNames and stored in colTypes_
+
+    for (size_t c = 0; c < ncoordcols_; c++)
+    {
+        if (colTypes_[c] == eGeoColStnId)
+            in >> stnid;
+        else if (colTypes_[c] == eGeoColLat)
+            in >> lat;
+        else if (colTypes_[c] == eGeoColLon)
+            in >> lon;
+        else if (colTypes_[c] == eGeoColLevel)
+            in >> level;
+        else if (colTypes_[c] == eGeoColDate)
+            in >> date;
+        else if (colTypes_[c] == eGeoColTime)
+            in >> time;
+    }
+
+    in >> value;  // try to read the first non-coordinate value (it may be present or missing)
+    if (!in)
+        value = 0; // no value? we probably want to at least plot the location, so set to something valid
+
+    if ( useProj4_) {
+        int error = pj_transform(proj4_, latlon_, 1, 1, &lon, &lat, NULL);    	
+        lon *= RAD_TO_DEG;
+        lat *= RAD_TO_DEG;
+    }	
+    UserPoint geo(lon, lat, value, value == missing_);
+    add(transformation, geo);
+}
+
+
+
+//_____________________________________________________________________
+// return the coordinate column map - if it's the first time, populate it
+// - this is a static function, and a static member variable
+
+std::map<std::string, GeoPointsDecoder::eGeoColType> GeoPointsDecoder::coordColMap_;
+
+const std::map<std::string, GeoPointsDecoder::eGeoColType> &GeoPointsDecoder::coordColMap()
+{
+    if (coordColMap_.empty())
+    {
+        coordColMap_["latitude"]  = eGeoColLat;
+        coordColMap_["longitude"] = eGeoColLon;
+        coordColMap_["level"]     = eGeoColLevel;
+        coordColMap_["date"]      = eGeoColDate;
+        coordColMap_["time"]      = eGeoColTime;
+        coordColMap_["stnid"]     = eGeoColStnId;
+    }
+    return coordColMap_;
+}
+
+
+//_____________________________________________________________________
+// The input line should be something like this:
+// stnid   latitude  time  longitude  date
+// and can contain, optionally, the names of the value columns, e.g.
+// stnid   latitude   time  longitude  date  temperature  ozone risk_factor
+// returns false if there was an error in parsing the line
+
+bool GeoPointsDecoder::parseColumnNames(const char *line)
+{
+    // tokenise into a list of strings
+    string sbuf(line);
+    vector<string> sv;
+    Tokenizer parse(" \t");
+    parse(sbuf,sv);
+
+    const std::map<std::string, eGeoColType> &colmap = coordColMap();
+
+
+    // for each string on the line
+    ncoordcols_ = 0;
+    bool valcols = false; // co-ordinate cols first, then value cols
+
+    for (size_t i = 0; i < sv.size(); i++)
+    {
+        std::string &name = sv[i];
+
+        // is this a standard co-ordinate column name?
+        map<string,eGeoColType>::const_iterator it = colmap.find(name);
+        if (it != colmap.end())
+        {
+            if (valcols)
+            {
+                MagLog::error() << "Error parsing geopoints #COLUMNS line: all co-ordinate columns must come before the value columns: "
+                                << name << " comes after a value column" << endl;
+                return false;
+            }
+            colTypes_.push_back(it->second);
+            ncoordcols_++;
+        }
+        else  // no, it must be a user-defined value name
+        {
+            valcols = true;
+        }
+    }
+
+
+    // check that the essential columns exist
+    if (std::find(colTypes_.begin(), colTypes_.end(), eGeoColLat) == colTypes_.end() ||
+        std::find(colTypes_.begin(), colTypes_.end(), eGeoColLon) == colTypes_.end())
+    {
+        MagLog::error() << "NCOLS-based geopoints must contain latitude and longitude columns" << endl;
+        return false;
+    }
+
+    return true;
+}
+
+
 void GeoPointsDecoder::decode(const Transformation& transformation)
 {
-	if ( formats_.empty() ) {
-		formats_["XYV"] = &GeoPointsDecoder::xyv2;
-		formats_["LLV"] = &GeoPointsDecoder::xyv2;
-		formats_["XY_VECTOR"] = &GeoPointsDecoder::lluv;
-		formats_["POLAR_VECTOR"] = &GeoPointsDecoder::polar;
-	}
-	useProj4_ = false;
-	GeoPointsDecoder::Decode method = &GeoPointsDecoder::yxdtlv2;
+    if ( formats_.empty() ) {
+        formats_["XYV"] = &GeoPointsDecoder::xyv2;
+        formats_["LLV"] = &GeoPointsDecoder::xyv2;
+        formats_["XY_VECTOR"] = &GeoPointsDecoder::lluv;
+        formats_["POLAR_VECTOR"] = &GeoPointsDecoder::polar;
+        formats_["NCOLS"] = &GeoPointsDecoder::ncols;
+    }
+    useProj4_ = false;
+    GeoPointsDecoder::Decode method = &GeoPointsDecoder::yxdtlv2;
 	
-	if ( !empty() ) return;
-	char line[1024];
-	try {
-		
-		ifstream in(path_.c_str());
-		while( in.getline( line, sizeof(line) ) ) {
-		      if ( strncmp( line, "#DATA", 5 ) == 0 ) 
-		    	  break;
-		      if ( strncmp( line, "#PROJECTION ", 12 ) == 0 ) {
-		      	useProj4_ = true;
-		      	string proj4(line+12);
-		      	
-		      	proj4_ = pj_init_plus(line+11);
-		      	latlon_ = pj_init_plus("+proj=longlat +ellps=WGS84 +datum=WGS84");
-		      }
-		      if ( strncmp( line, "#FORMAT ", 8 ) == 0 ) {
-		    	  const char* fp = line+7;
-		    	  while( fp && *fp == ' ' )
-		    		  	++fp;
-		    	  string format(fp);
-		    	
-		    	  map<string, GeoPointsDecoder::Decode>::iterator m = formats_.find(format);
-		    	  method = ( m != formats_.end() ) ? m->second : &GeoPointsDecoder::yxdtlv2;
-		      }
-		    }
+    if ( !empty() ) return;
+    char line[1024];
+    try {
+        ifstream in(path_.c_str());
+        while( in.getline( line, sizeof(line) ) ) {
+            if ( strncmp( line, "#DATA", 5 ) == 0 ) 
+                break;
+            if ( strncmp( line, "#PROJECTION ", 12 ) == 0 ) {
+                useProj4_ = true;
+                string proj4(line+12);
 
-		  while( in.getline( line, sizeof(line) ) ) {
-		      if( (*line != '#') && (strlen( line ) > 4) ) {
-		    	  (this->*method)(line, transformation);
-		      }
-		    }
+                proj4_ = pj_init_plus(line+11);
+                latlon_ = pj_init_plus("+proj=longlat +ellps=WGS84 +datum=WGS84");
+            }
+            if ( strncmp( line, "#FORMAT ", 8 ) == 0 ) {
+                const char* fp = line+7;
+                while( fp && *fp == ' ' )
+                    ++fp;
+                string format(fp);
 
-		//computeStats();
+                map<string, GeoPointsDecoder::Decode>::iterator m = formats_.find(format);
+                method = ( m != formats_.end() ) ? m->second : &GeoPointsDecoder::yxdtlv2;
+            }
+            if ( strncmp( line, "#COLUMNS ", 7 ) == 0 ) {
+                if (method == &GeoPointsDecoder::ncols)
+                {
+                    in.getline( line, sizeof(line) );
+                    parseColumnNames(line);
+                }
+            }
+        }
 
-		in.close();
-	}
-	catch (...)
-	{
-		MagLog::error() << "Geopoints file : can not open " << path_ << endl;
-	}
-	 
-	 
+        while( in.getline( line, sizeof(line) ) ) {
+            if( (*line != '#') && (strlen( line ) > 4) ) {
+                (this->*method)(line, transformation);
+            }
+        }
+
+        //computeStats();
+
+        in.close();
+    }
+    catch (...)
+    {
+        MagLog::error() << "Geopoints file : can not open " << path_ << endl;
+    }
 }
 
 void GeoPointsDecoder::customisedPoints(const Transformation& transformation, const std::set<string>&, CustomisedPointsList& list)
