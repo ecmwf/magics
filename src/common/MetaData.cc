5f5b5a66354c599e0f7886f8b4b6f30ce6e133ff
Daniel Tipping <daniel@oldreliable.tech>
2018-12-15 15:03:38 +0000

    Remove unused variables

--- src/common/MetaData.cc
+++ src/common/MetaData.cc
@@ -79,9 +79,6 @@ void MetaDataVisitor::collectMetaData()
 	try {
 	 ofstream out(profile_.c_str());
 
-	    	double t;
-	       struct timeval tim;
-
 		out << "{\n\"timers\" : {" << endl;
 
 		string s = "";
