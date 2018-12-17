c7a02e3d7a9a76a88361618e84b0d43c8fe53b87
Daniel Tipping <daniel@oldreliable.tech>
2018-12-17 18:40:27 +0000

    Windows: include functional in magics_windef.h

--- src/magics_windef.h
+++ src/magics_windef.h
@@ -19,6 +19,7 @@
    * to each file as required, but it's more straightforward to put it here.
    */
   #include <algorithm>
+  #include <functional> // for std::greater
   #ifndef M_PI
     #define M_PI   3.14159265358979323846
     #define M_PI_2 1.57079632679489661923
