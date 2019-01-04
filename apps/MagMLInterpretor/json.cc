5724e906714c7b78f7e205fae6862df90bc20718
Daniel Tipping <daniel@oldreliable.tech>
2019-01-04 17:44:58 +0000

    Windows: bypass SAGALRM usage
    This probably needs porting.

--- apps/MagMLInterpretor/json.cc
+++ apps/MagMLInterpretor/json.cc
@@ -86,10 +86,14 @@ int normal_main(int argc, char **argv)
             map<string, string>::const_iterator out = WebInterpretor::parameters().find("timeout");
             int timeout = (out == WebInterpretor::parameters().end() ) ? 0 : tonumber(out->second);
             MagLog::info() << "Time out armed for " << timeout << endl;
+#ifndef MAGICS_ON_WINDOWS
             signal(SIGALRM, catch_alarm);
             alarm(timeout);
+#endif
             WebInterpretor::json(argv[1]);
+#ifndef MAGICS_ON_WINDOWS
             alarm(0);
+#endif
 		}
 		catch (...) {
 			std::cout << argv[0] << " FAILED to dispatch JSON file!"<< endl;
