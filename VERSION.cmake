9e718b1d6669d55513287840d77017cc1770375c
Sylvie Lamy-Thepaut <Sylvie.Lamy-Thepaut@ecmwf.int>
2019-01-29 08:20:10 +0000

    Python OFF

--- VERSION.cmake
+++ VERSION.cmake
@@ -7,8 +7,8 @@
 # does it submit to any jurisdiction.
 
 
-set ( metabuilder_version 3.4.0.9 )
-set ( _version 3.4.0.9 )
+set ( metabuilder_version 3.4.0.10 )
+set ( _version 3.4.0.10 )
 
 
 if ( MAGICS_BUILD )
