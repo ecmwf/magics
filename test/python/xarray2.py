84666b95e8639808b38366822a3bc245770ea3a7
Daniel Tipping <daniel@oldreliable.tech>
2019-04-16 14:53:49 +0100

    Test: use keyword args in xarray tests

--- test/python/xarray2.py
+++ test/python/xarray2.py
@@ -1,8 +1,8 @@
 # (C) Copyright 1996-2019 ECMWF.
-# 
+#
 # This software is licensed under the terms of the Apache Licence Version 2.0
-# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
-# In applying this licence, ECMWF does not waive the privileges and immunities 
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
 # granted to it by virtue of its status as an intergovernmental organisation nor
 # does it submit to any jurisdiction.
 
@@ -21,6 +21,9 @@ contour = magics.mcont(contour_automatic_setting = "ecmwf")
 
 magics.plot(
         png,
-        magics.mxarray(ds, "z", time=np.datetime64('2017-10-13T00:00:00.000000000'), level=925),
+        magics.mxarray(ds = ds,
+            var = "z",
+            time = np.datetime64('2017-10-13T00:00:00.000000000'),
+            level = 925),
         contour,
         magics.mcoast())
