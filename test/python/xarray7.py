6ea8a50af285403b2afbdc977349ea40d705d760
Daniel Tipping <daniel@oldreliable.tech>
2019-04-23 11:37:55 +0100

    Add failing test xarray7.py

---
+++ test/python/xarray7.py
@@ -0,0 +1,23 @@
+# (C) Copyright 1996-2019 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation nor
+# does it submit to any jurisdiction.
+
+import cftime
+import xarray as xr
+import numpy as np
+
+from Magics import macro as magics
+
+ref = "xarray7"
+
+ds = xr.open_dataset('C3S_OZONE-L3-LP_CONC_MZM-GOMOS_ENVISAT_CCI-200711-fv0001.nc')
+
+png = magics.output(output_name_first_page_number = "off", output_name = ref)
+
+contour = magics.mcont(contour_automatic_setting = "ecmwf")
+
+magics.plot(png, magics.mxarray(ds = ds, var = "pressure"), contour, magics.mcoast())
