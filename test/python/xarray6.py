c9ce267bae3f771b2cd2ea4da8c3453a10b3f41d
Daniel Tipping <daniel@oldreliable.tech>
2019-04-24 19:33:07 +0100

    xarray: update tests to new argument names

--- test/python/xarray6.py
+++ test/python/xarray6.py
@@ -6,7 +6,6 @@
 # granted to it by virtue of its status as an intergovernmental organisation nor
 # does it submit to any jurisdiction.
 
-#l_Amon_GFDL-ESM2M_rcp45_r1i1p1_208101-208512.nc
 import cftime
 import xarray as xr
 import numpy as np
@@ -14,16 +13,14 @@ import numpy as np
 from Magics import macro as magics
 
 ref = "xarray6"
-
-
-
-
 ds = xr.open_dataset('psl_Amon_GFDL-ESM2M_rcp45_r1i1p1_208101-208512.nc')
-
-png = magics.output(output_name_first_page_number = "off", output_name = ref)
-
-contour = magics.mcont(contour_automatic_setting = "ecmwf")
-
 time = cftime.DatetimeNoLeap(2085, 12, 16, 12, 0, 0, 0, 5, 350)
 
-magics.plot(png, magics.mxarray(ds = ds, var = "psl", time = time), contour, magics.mcoast())
+png = magics.output(output_name_first_page_number = "off", output_name = ref)
+data = magics.mxarray(
+        xarray_dataset = ds,
+        xarray_variable_name = "psl",
+        xarray_dimension_settings = {"time": time})
+contour = magics.mcont(contour_automatic_setting = "ecmwf")
+
+magics.plot(png, data, contour, magics.mcoast())
