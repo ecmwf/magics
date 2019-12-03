1f5beeb4d06d3c8e35ddf379b43584ba64ee773a
Ian Vermes <ian.vermes@gmail.com>
2019-12-03 20:06:31 +0000

    Add debuging echos

--- check_formatting.sh
+++ check_formatting.sh
@@ -3,11 +3,15 @@
 # Auto-formatting script that checks whether the CWD and child directories have
 # formatted source code.
 
+echo $(clang-format --version)
+
 find . -iname *.h -o -iname *.c -o -iname *.cpp -o -iname *.cc -o -iname *.hpp | while read path; do
     ORIG=$(cat $path)
     # The `--style=file` arguments while automatically find the .clang-format
     # config file. It doesn't take a file path but just the string `file`
     # If .clang-format is missing the script will exit.
     AUTO=$(clang-format --style=file --fallback-style=none $path)
-    diff <(echo $ORIG) <(echo $AUTO) > /dev/null || { echo "$path is not formatted, format with clang-format"; exit 1; }
+    diff <(echo $ORIG) <(echo $AUTO) > /dev/null || echo "$path is not formatted, format with clang-format"
 done
+
+exit 1
