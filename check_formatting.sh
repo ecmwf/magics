97da201ee1d5b1d5b8b09b9ddd574d8e91362b52
Ian Vermes <ian.vermes@gmail.com>
2019-12-10 12:54:03 +0000

    Simplify `find` arguments for readability, reproducability & use quotes

--- check_formatting.sh
+++ check_formatting.sh
@@ -2,11 +2,11 @@
 
 # Auto-formatting script that checks whether the CWD and child directories have
 # formatted source code.
-find . -iname *.h -o -iname *.c -o -iname *.cpp -o -iname *.cc -o -iname *.hpp | while read path; do
-    ORIG=$(cat $path)
+find -E . -regex '.*\.(cc|cpp|hpp|cxx|h|c)' | while read path; do
+    ORIG=$(cat "$path")
     # The `--style=file` arguments while automatically find the .clang-format
     # config file. It doesn't take a file path but just the string `file`
     # If .clang-format is missing the script will exit.
-    AUTO=$(clang-format --style=file --fallback-style=none $path)
-    diff <(echo $ORIG) <(echo $AUTO) > /dev/null ||{ echo "$path is not formatted"; exit 1; }
+    AUTO=$(clang-format --style=file --fallback-style=none "$path")
+    diff <(echo "$ORIG") <(echo "$AUTO") > /dev/null ||{ echo "$path is not formatted"; exit 1; }
 done
