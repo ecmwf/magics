425930e99e3deab82ccd372ebcfec8a0eb9e00cd
Ian Vermes <ian.vermes@gmail.com>
2019-12-10 13:41:28 +0000

    Improve script documentation

--- check_formatting.sh
+++ check_formatting.sh
@@ -4,9 +4,10 @@
 # formatted source code.
 find -E ./src -regex '.*\.(cc|cpp|hpp|cxx|h)' | while read path; do
     ORIG=$(cat "$path")
-    # The `--style=file` arguments while automatically find the .clang-format
-    # config file. It doesn't take a file path but just the string `file`
+    # The `--style=file` argument will automatically find the .clang-format and
+    # it doesn't take a file path but just the string `file`.
     # If .clang-format is missing the script will exit.
+    # The `--style=llvm` will use the llvm standard style.
     AUTO=$(clang-format --style=llvm --fallback-style=none "$path")
     diff <(echo "$ORIG") <(echo "$AUTO") > /dev/null ||{ echo "$path is not formatted"; exit 1; }
 done
