a7997c57b55258105cb2a35922a04f992001057a
Ian Vermes <ian.vermes@gmail.com>
2019-12-10 13:41:28 +0000

    Improve script documentation

--- check_formatting.sh
+++ check_formatting.sh
@@ -4,8 +4,8 @@
 # formatted source code.
 find -E . -regex '.*\.(cc|cpp|hpp|cxx|h|c)' | while read path; do
     ORIG=$(cat "$path")
-    # The `--style=file` arguments while automatically find the .clang-format
-    # config file. It doesn't take a file path but just the string `file`
+    # The `--style=file` argument will automatically find the .clang-format and
+    # it doesn't take a file path but just the string `file`.
     # If .clang-format is missing the script will exit.
     AUTO=$(clang-format --style=file --fallback-style=none "$path")
     diff <(echo "$ORIG") <(echo "$AUTO") > /dev/null ||{ echo "$path is not formatted"; exit 1; }
