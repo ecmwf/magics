4ab3f288df58533f7019933f1903b9143a5879c6
Daniel Tipping <daniel@oldreliable.tech>
2019-12-11 17:20:50 +0000

    Rename $path to $file

--- check_formatting.sh
+++ check_formatting.sh
@@ -7,17 +7,17 @@
 unameOut="$(uname -s)"
 base_dir="."
 case "${unameOut}" in
-    Darwin*)    all_paths=$(find -E $base_dir -regex '.*\.(cpp|hpp|cc|cxx|h|c)');;
-    Linux*)     all_paths=$(find $base_dir -regex '.*\.\(cpp\|hpp\|cc\|cxx\|h\|c\)')
+    Darwin*)    files=$(find -E $base_dir -regex '.*\.(cpp|hpp|cc|cxx|h|c)');;
+    Linux*)     files=$(find $base_dir -regex '.*\.\(cpp\|hpp\|cc\|cxx\|h\|c\)')
 esac
 
-for path in $all_paths; do
-    ORIG=$(cat "$path")
+for file in $files; do
+    ORIG=$(cat "$file")
     # The `--style=file` argument will automatically find the .clang-format and
     # it doesn't take a file path but just the string `file`.
     # If .clang-format is missing the script will exit.
-    AUTO=$(clang-format --style=file --fallback-style=none "$path")
-    diff <(echo "$ORIG") <(echo "$AUTO") > /dev/null || { echo "$path is not formatted"; exit 1; }
+    AUTO=$(clang-format --style=file --fallback-style=none "$file")
+    diff <(echo "$ORIG") <(echo "$AUTO") > /dev/null || { echo "$file is not formatted"; exit 1; }
 done
 
 echo "Format check complete"
