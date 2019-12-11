8e3f3effb2daae822182d1792dd063dc4a7a7615
Daniel Tipping <daniel@oldreliable.tech>
2019-12-11 17:46:44 +0000

    Skip formatting check for drivers/minizip

--- check_formatting.sh
+++ check_formatting.sh
@@ -12,12 +12,18 @@ case "${unameOut}" in
 esac
 
 for file in $files; do
-    ORIG=$(cat "$file")
-    # The `--style=file` argument will automatically find the .clang-format and
-    # it doesn't take a file path but just the string `file`.
-    # If .clang-format is missing the script will exit.
-    AUTO=$(clang-format --style=file --fallback-style=none "$file")
-    diff <(echo "$ORIG") <(echo "$AUTO") > /dev/null || { echo "$file is not formatted"; exit 1; }
+    if [[ $file =~ "drivers/minizip" ]]; then
+        # Skip minizip because clang-format breaks this code to the point where it doesn't
+        # compile.
+        echo "Skipping autoformat check for file: $file"
+    else
+        ORIG=$(cat "$file")
+        # The `--style=file` argument will automatically find the .clang-format and
+        # it doesn't take a file path but just the string `file`.
+        # If .clang-format is missing the script will exit.
+        AUTO=$(clang-format --style=file --fallback-style=none "$file")
+        diff <(echo "$ORIG") <(echo "$AUTO") > /dev/null || { echo "$file is not formatted"; exit 1; }
+    fi
 done
 
 echo "Format check complete"
