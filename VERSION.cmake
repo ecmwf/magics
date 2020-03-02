47f9e1efbbce34c1195fc131aea79fe395578c17
Sylvie Lamy-Thepaut <Sylvie.Lamy-Thepaut@ecmwf.int>
2020-03-02 10:45:29 +0000

    Update version number 4.3.0

--- VERSION.cmake
+++ VERSION.cmake
@@ -7,8 +7,8 @@
 # does it submit to any jurisdiction.
 
 
-set ( metabuilder_version 4.2.6 )
-set ( _version 4.2.6 )
+set ( metabuilder_version 4.3.0 )
+set ( _version 4.3.0 )
 
 
 if ( MAGICS_BUILD )
