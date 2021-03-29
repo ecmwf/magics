39b089b4457f338b44c98d38d7b860f423863527
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2021-03-29 10:57:44 +0100

    No CSS colours

--- src/common/Colour.cc
+++ src/common/Colour.cc
@@ -382,7 +382,7 @@ void Colour::init() {
     colours_["white"]            = Rgb(1.0000, 1.0000, 1.0000);
     colours_["undefined"]        = Rgb(-1., -1., -1.);
 
-#if 1
+#if 0
     // CSS colours
     colours_["aliceblue"]            = Colour("#F0F8FF");
     colours_["antiquewhite"]         = Colour("#FAEBD7");
