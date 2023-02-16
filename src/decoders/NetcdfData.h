30084f32df0184c9be78e5793973c1d20dacfb72
Sylvie Lamy-Thepaut <sylvie.lamy-thepaut@ecmwf.int>
2023-02-16 11:32:05 +0000

    Netcdf Plotting : fix bug in the interpretation of the date in hivmoller plots type

--- src/decoders/NetcdfData.h
+++ src/decoders/NetcdfData.h
@@ -476,6 +476,8 @@ public:
     }
 
     map<string, NetAttribute> getAttributes() { return attributes_; }
+    void ignoreDimension(const string& dim) { ignoredDimensions_.push_back(dim); }
+    void interpretDimension(const string& dim) { std::remove( ignoredDimensions_.begin(), ignoredDimensions_.end(), dim ); }
 
 
 protected:
@@ -486,6 +488,7 @@ protected:
     map<string, NetAttribute> attributes_;
     double missing_;
     mutable map<string, string> detected_;
+    vector <string> ignoredDimensions_; 
 
 private:
     int file_;
